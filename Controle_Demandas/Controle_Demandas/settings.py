280ddca75d45669a8c4389ecb6894fa24b07c38f
Gabriel Bravo Antonio Silva <gabrielbravo4321@gmail.com>
2025-08-22 11:18:58 -0300

    first commit

--- Controle_Demandas/Controle_Demandas/settings.py
+++ Controle_Demandas/Controle_Demandas/settings.py
@@ -128,3 +128,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# settings.py
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
