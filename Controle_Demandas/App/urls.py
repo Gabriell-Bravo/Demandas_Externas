db343c96025232f2bc7f665d6a17d3363b20967b
Gabriel Bravo Antonio Silva <gabrielbravo4321@gmail.com>
2025-08-22 12:11:49 -0300

    first commit

--- Controle_Demandas/App/urls.py
+++ Controle_Demandas/App/urls.py
@@ -3,9 +3,7 @@ from . import views
 from django.views.generic.base import RedirectView
 
 urlpatterns = [
-    path('Login/', views.login_view, name='exibir_login'),
-    path('Cadastro_Demandas/', views.Exibir_Cadastro_Demandas,
-         name='Exibir_Cadastro_demandas'),
+    path('', views.login_view, name='exibir_login'),
+    path('Cadastro_Demandas/', views.Exibir_Cadastro_Demandas, name='Exibir_Cadastro_demandas'),
     path('Painel/', views.Exibir_Painel, name='Exibir_Painel'),
-    path('', RedirectView.as_view(url='Login/', permanent=False)),
 ]
